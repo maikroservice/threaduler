46a2d625305b48c64d5bbe33159f0018c4b49b20
maikroservice <20245897+maikroservice@users.noreply.github.com>
2023-07-31 23:53:03 +0200

    working prototype v1

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import fastapi
 from models import Status, Tweet, TweetThread
 import requests
 import uuid 
+import tweepy
+import tempfile
+from dotenv.main import load_dotenv
+import os
 
-NOTION_TOKEN="secret_fv2dLcfGzqYNKXmXsRIaXaDw0KWOkn1FilunNVo1C4Y"
-# old database
-#NOTION_DATABASE_ID="b38f425a0f7c4f23ac6c821f7a5b6075"
+load_dotenv()
 
-# thread database
-#NOTION_DATABASE_ID="b4f76b8a213d42eb8366bc759e3e1cfd"
-NOTION_DATABASE_ID="c57dc46a884c49bf97bdb333b4d117a8"
+
+NOTION_TOKEN=os.environ["NOTION_API_TOKEN"]
+
+NOTION_DATABASE_ID=os.environ["NOTION_DATABASE_ID"]
 NOTION_VERSION = "2022-06-28"
 
 app = fastapi.FastAPI()
@@ -54,7 +57,7 @@ async def schedule():
 
 
 @app.get("/tweets/{page_id}")
-async def show_scheduled_tweet(page_id):
+async def transform_notion_to_tweet(page_id):
         url = f'https://api.notion.com/v1/blocks/{page_id}/children'
         r = requests.get(url, headers={
         "Authorization": f"Bearer {NOTION_TOKEN}",
@@ -62,25 +65,50 @@ async def show_scheduled_tweet(page_id):
         
         })
         blocks = r.json()["results"]
+        print(blocks)
         tweet = {}
         
-        tweet["content"] = ''
-        tweet["media"] = {}
+        tweet["tweet"] = ''
+        tweet["media"] = []
         
 
         
-        paragraph_blocks = [block["paragraph"]["rich_text"] for block in blocks if block["type"]=="paragraph"]
+        paragraph_blocks = [block["paragraph"]["rich_text"] for block in blocks if block["type"] == "paragraph"]
         for block in paragraph_blocks:
             if block:
-                tweet["content"] += f"{block[0]['plain_text'].rstrip()}\n"
+                tweet["tweet"] += f"{block[0]['plain_text'].rstrip()}\n"
             else:
-                tweet["content"] += "\n\n"
+                tweet["tweet"] += "\n\n"
+        
+        media_blocks = [block["image"] for block in blocks if block["type"] == "image"]
+        for i, block in enumerate(media_blocks):
+            if block:
+                tweet["media"].append({"fileUrl": block["file"]["url"], "mimeType": "image/png"})
+                
 
         return tweet
 
+@app.get("/publish/{page_id}")
+async def publish_tweet(page_id):
+    content = await transform_notion_to_tweet(page_id)
+
+    CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
+    CONSUMER_SECRET = os.environ["TWITTER_CONSUMER_SECRET"]
+    ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
+    ACCESS_TOKEN_SECRET = os.environ["TWITTER_TOKEN_SECRET"]
+
+    client = tweepy.Client(
+    consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
+    access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET
+    )
+
+    #media = [await client.upload_media(image["fileUrl"]).media_key for image in content["media"] if image]
+    response = client.create_tweet(text=content["tweet"])
+    print(f"https://twitter.com/user/status/{response.data['id']}")
+
 @app.get("/update")
 # write data to notion?/database / sync current likes/retweets etc 
-async def root():
+async def update_notion_db():
     url = f'https://api.notion.com/v1/pages/'
     r = requests.post(url, headers={
         "Authorization": f"Bearer {NOTION_TOKEN}",
